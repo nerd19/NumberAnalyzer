79c7d0179e21a6560706c669416764392266b350
Nicole Binette <35899872+nerd19@users.noreply.github.com>
2018-03-13 15:16:15 -0700

    Add files via upload

--- NumberAnalyzer.java
+++ NumberAnalyzer.java
@@ -1,39 +1,90 @@
+/**
+ * @author Nicole Binette
+ * 3/7/2018
+ * Assignment 5
+ */
+import java.util.Arrays;
+/**
+ * A generic class that initializes an Array as a type class of Numbers or type subclass of Numbers
+ * Methods include greatestValue, lowestValue, average, total, and newArray
+ */
 public class NumberAnalyzer<T extends Number> {
+    // Array that holds the numbers of type class Numbers or type subclass of Numbers. 
     private T numbers [];
+    /**
+     * Constructor.
+     * Initializes array of type class Numbers or type subclass of Numbers.
+     * @param T a[] a generic array
+     */
     public NumberAnalyzer(T a[]) {
+        // Initialize array (should be the same length as the array passed as argument)
+        numbers = newArray(a.length);
+        // 
         for (int index = 0; index < a.length; index++) {
             numbers[index] = a[index];
         }
     }
+    /**
+     * Method finds the greatest value in the Array
+     * @param T a[] a generic array
+     * @return T greatest value stored in the Array
+     */
     public T greatestValue(T a[]) {
         T greatestValue = a[0];
+        //int greatestIndex = 0;
         for (int index = 0; index < a.length; index++) {
-            if (a[index] > greatestValue) {
+            if (a[index].doubleValue() > greatestValue.doubleValue()) {
                 greatestValue = a[index];
             }
         }
         return greatestValue;
     }
+    /**
+     * Method finds the lowest value in the Array
+     * @param T a[] a generic array
+     * @return T lowest value stored in the Array
+     */
     public T lowestValue(T a[]) {
         T lowestValue = a[0];
+        //int lowestIndex = 0;
         for (int index = 0; index < a.length; index++) {
-            if (a[index] < lowestValue) {
+            if (a[index].doubleValue() < lowestValue.doubleValue()) {
                 lowestValue = a[index];
             }
         }
+        return lowestValue;
     }
-    public T average(T a[]) {
-        T average;
+    /**
+     * Method calculates the average of all the elements in the Array
+     * @param T a[] a generic array
+     * @return double average
+     */
+    public double average(T a[]) {
+        double average = 0;
+        double total = 0;
         for (int index = 0; index < a.length; index++) {
-            average = average + a[index];
+            total += (Double) a[index].doubleValue();
         }
-        return average/a.length;
+        return average = (Double) total/a.length;
     }
-    public int count(T a[]) {
-        int counter = 0;
+    /**
+     * Method calculates the total of all elements in the Array
+     * @param T a[] a generic array
+     * @return double total
+     */
+    public double total(T a[]) {
+        double total = 0;
         for (int index = 0; index < a.length; index++) {
-            counter++;
+            total += (Double) a[index].doubleValue();
         }
-        return counter;
+        return total;
+    }
+    /**
+     * Method creates a copy of type class Numbers or subclass of Numbers
+     */
+    @SafeVarargs
+    static <T> T[] newArray(int length, T... array)
+    {
+        return Arrays.copyOf(array, length);
     }
 }
--- NumberAnalyzerDemo.java
+++ NumberAnalyzerDemo.java
@@ -1,10 +1,32 @@
+/**
+ * @author Nicole Binette
+ * 3/7/2018
+ * Assignment 5
+ */
 public class NumberAnalyzerDemo {
     public static void main(String args[]) {
-        Integer i[] = {1,2,3,4,5};
-        Double d[] = {0.0, 1.2, 4.5, 8.0};
-        NumberAnalyzer n = new NumberAnalyzer<>(i);
-        NumberAnalyzer m = new NumberAnalyzer<>(d);
-        System.out.println(greatestValue(n));
-        System.out.println(greatestValue(m));
+        // Primitive arrays
+        int a[] = {0,1,2,3,4};
+        double d[] = {0.0, 1.0, 2.0, 3.0, 4.0};
+        // Wrapper class arrays
+        Integer b[] = new Integer[a.length];
+        Double c[] = new Double[d.length];
+        // convert primitive arrays to Wraper class arrays
+        for (int index = 0; index < b.length; index++) {
+            b[index] = a[index];
+            c[index] = d[index];
+        }
+        // create an instance of NumberAnalyzer class
+        NumberAnalyzer<Double> g = new NumberAnalyzer<>(c);
+        NumberAnalyzer<Integer> f = new NumberAnalyzer<>(b);
+        // test methods from NumberAnalyzer class
+        System.out.println(g.greatestValue(c));
+        System.out.println(g.lowestValue(c));
+        System.out.println(g.average(c));
+        System.out.println(g.total(c));
+        System.out.println(f.greatestValue(b));
+        System.out.println(f.lowestValue(b));
+        System.out.println(f.average(b));
+        System.out.println(f.total(b));
     }
 }
